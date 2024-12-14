d7e4732c4e33704601903e85164a8040929e2a0a
Doğaç Ulğaç <ulgac1907@gmail.com>
2024-12-14 15:05:37 +0300

    CustomerService updated

--- src/main/java/com/enoca/enocaTask/service/CustomerService.java
+++ src/main/java/com/enoca/enocaTask/service/CustomerService.java
@@ -4,14 +4,18 @@ import com.enoca.enocaTask.dto.CustomerDto;
 import com.enoca.enocaTask.entity.Cart;
 import com.enoca.enocaTask.entity.Customer;
 import com.enoca.enocaTask.repository.CustomerRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final CartService cartService;
+
     public CustomerService(CustomerRepository customerRepository, CartService cartService) {
         this.customerRepository = customerRepository;
         this.cartService = cartService;
@@ -32,12 +36,16 @@ public class CustomerService {
         cart.setTotalPrice(0.0); // Başlangıçta toplam fiyatı sıfır
 
         // Alışveriş sepetini veritabanına ekle
-         cartService.addCart(cart);
+        cartService.addCart(cart);
         return customerRepository.getReferenceById(customer.getId());
     }
 
     public Customer updateUser(Long userId, CustomerDto newUser) {
-    Customer updateCustomer = customerRepository.getReferenceById(userId);
+        Optional<Customer> optionalCustomer = customerRepository.findById(userId);
+        if (optionalCustomer.isEmpty()) {
+            throw new EntityNotFoundException("Customer with ID " + userId + " not found");
+        }
+        Customer updateCustomer = optionalCustomer.get();
         updateCustomer.setEmail(newUser.getEmail());
         return customerRepository.save(updateCustomer);
     }
